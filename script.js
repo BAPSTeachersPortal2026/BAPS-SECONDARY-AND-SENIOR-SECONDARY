0e2b247527ba00cdc585960234fa7881fa94ee4e
BAPS - SENIOR SECONDARY  WING <saraswathid@bapssathy.ac.in>
2026-06-10 12:39:03 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function filterSubjects(){
-let f=document.getElementById('search').value.toLowerCase();
-document.querySelectorAll('#subjectList li').forEach(li=>{
-li.style.display=li.textContent.toLowerCase().includes(f)?'':'none';
-});
-}
